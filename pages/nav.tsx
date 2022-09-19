f88661d2b364d921bbade5086e021d830418ae72
fio.ucs <fio.ucs@kakaoenterprise.com>
2022-09-19 17:53:21 +0900

    .

--- pages/nav.tsx
+++ pages/nav.tsx
@@ -1,9 +1,14 @@
-import type {NextPage} from "next";
+import type {NextPage} from "next"
+import Image from 'next/image'
+import homeImg from '../public/fragranceia.png'
 
 const Nav: NextPage = () => {
-    return (
+  return (
+      <div>
+        <Image src={homeImg} width={150} height={1000} layout={"fixed"}/>
         <div>Nav</div>
-    )
+      </div>
+  )
 }
 
 

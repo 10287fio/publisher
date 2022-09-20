d0ac2a27168f5d9128d4a143b70597c11795a64b
fio.ucs <fio.ucs@kakaoenterprise.com>
2022-09-20 14:09:18 +0900

    .

--- pages/nav.tsx
+++ pages/nav.tsx
@@ -1,11 +1,18 @@
 import type {NextPage} from "next"
 import Image from 'next/image'
 import homeImg from '../public/fragranceia.png'
+import Link from 'next/link'
 
 const Nav: NextPage = () => {
   return (
-      <div>
-        <Image src={homeImg} width={150} height={1000} layout={"fixed"}/>
+      <div style={{display:"inline-block"}}>
+        <Link href="/nav">
+          <a>
+            <div style={{display:"inline-block", width:"100px", height:"100px"}}>
+              <Image src={homeImg} width={200} height={200} layout={"fixed"}/>
+            </div>
+          </a>
+        </Link>
         <div>Nav</div>
       </div>
   )

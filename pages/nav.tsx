2fbabcad2d26db8cf9b3d361fbaeef7b938d1b85
fio.ucs <fio.ucs@kakaoenterprise.com>
2022-09-27 15:02:47 +0900

    .

--- pages/nav.tsx
+++ pages/nav.tsx
@@ -24,7 +24,7 @@ const Nav: NextPage = () => {
             </a>
           </Link>
         </div>
-        <div style={{flexBasis: '100%'}}>
+        <div style={{flexBasis: '50%'}}>
           <div style={{
             display: 'flex',
             flexDirection: 'row',

5c2dd22c7736a989e17dc407b39a6323015397ad
fio.ucs <fio.ucs@kakaoenterprise.com>
2023-06-13 17:29:56 +0900

    .

--- layout/footer.tsx
+++ layout/footer.tsx
@@ -2,7 +2,7 @@ import type {NextPage} from 'next'
 import langStyles from '@/layout/styles/layout.module.scss'
 
 const Footer: NextPage = () => {
-    return <div className={langStyles.footer}>Footer</div>
+    return <div className={langStyles.footer}>© 2023 Fragranceia, Co.</div>
 }
 
 export default Footer

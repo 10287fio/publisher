486c8554ce4e44440eb05a3cbc780f3669d0df98
fio.ucs <fio.ucs@kakaoenterprise.com>
2023-06-13 10:43:20 +0900

    .

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,10 @@ import type {NextPageWithLayout} from './_app';
 
 const Home: NextPageWithLayout = () => {
     return (
-        <div style={{textAlign: 'center'}}>
-
+        <div style={{textAlign: 'center', whiteSpace:'pre-line'}}>
+            It could be a ZIN to call the universe&#039;s smallest unit&#46;<br/>
+            ZIN is a unit which couldn&#039;t split any more&#46;<br/>
+            Zin is a unit which couldn&#039;t mix the other material&#46;<br/>
         </div>
     )
 }

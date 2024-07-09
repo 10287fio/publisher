ca6af2126a66dcf77544d1d739fc486c9dee1630
10287fio <10287fio@gmail.com>
2024-07-09 23:47:13 +0900

    .

--- composition/layout/nav.tsx
+++ composition/layout/nav.tsx
@@ -8,6 +8,9 @@ import {RootState} from "@/store"
 import fragranceiaImg from "@/resource/Fragranceia.png"
 import Dropdown from "@/component/dropdown/Dropdown";
 import ManyLang from "@/store/enum/manyLang";
+import Link from "next/link";
+import {router} from "next/client";
+import {redirect} from "next/navigation";
 
 const Nav: NextPage = () => {
     const customUseSelector: TypedUseSelectorHook<RootState> = useSelector
@@ -17,19 +20,28 @@ const Nav: NextPage = () => {
         a[c.key] = {name: c.value, url: c.url}
         return a
     }, {})
+    const home = () => {
+        redirect('/')
+    }
 
     return (
         <div className={layoutStyles.nav}>
             <div className={layoutStyles.navLayout}>
                 <div className={layoutStyles.navItemHome}>
-                    <div style={{cursor: 'pointer'}}>
+                    {/*<div onClick={home} style={{cursor: 'pointer'}}>*/}
+                    <Link href={"/"}>
                         <Image
                             src={fragranceiaImg}
-                            width={100} height={100} layout={'responsive'} alt={'Fragranceia'}/>
-                    </div>
+                            width={100} height={100} alt={'Fragranceia'} priority={false} placeholder="empty"/>
+                    {/*</div>*/}
+                    </Link>
                 </div>
                 <div className={layoutStyles.navMargin1}></div>
                 <div className={layoutStyles.navItem}>
+                    {navItems.map((item) =>
+                        <div key={item.key.toString()}>
+                            <Link href={navItemsLang[item.key].url}>{item.value}</Link>
+                        </div>)}
                 </div>
                 <div className={layoutStyles.navMargin2}></div>
                 <div className={layoutStyles.navItemLang}>

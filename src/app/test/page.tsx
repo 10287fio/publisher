3bd4428f27074f5f76409a596e13c5c5e977f8e0
10287fio <10287fio@gmail.com>
2024-07-20 23:50:31 +0900

    .

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,4 +1,4 @@
-import Checkbox from '@/component/checkbox/Checkbox'
+import Checkbox from '@/component/checkbox/Checkbox';
 
 export default function test1():JSX.Element {
     return <div><Checkbox/></div>

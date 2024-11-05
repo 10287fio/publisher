c2bd1df6a00d65897e76c541cf892939cb9d7c71
fio.ucs <fio.ucs@kakaoenterprise.com>
2024-11-05 22:27:16 +0900

    .

--- src/app/manuscript/page.tsx
+++ src/app/manuscript/page.tsx
@@ -10,18 +10,33 @@ export default function Manuscript(): JSX.Element {
         if(canvasRef.current){
             const canvas = canvasRef.current;
             if(canvas.getContext){
-                const context = canvas.getContext("2d");
+                const ctx = canvas.getContext("2d");
 
-                if(context){
-                    context.strokeStyle = "blue";
-                    context.strokeRect(0, 0, 150, 150);
+                let offsetX;
+                let offsetY;
+
+                if(ctx){
+                    ctx.strokeStyle = "blue";
+                    ctx.strokeRect(0, 0, 150, 150);
+                    canvas.addEventListener("mousemove", (e:MouseEvent) => {
+                        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+                        offsetX = e.offsetX;
+                        offsetY = e.offsetY;
+
+                        ctx.beginPath();
+                        ctx.moveTo(0, 0);
+                        ctx.lineTo(offsetX, offsetY);
+                        ctx.closePath();
+                        ctx.stroke();
+                    });
                 }
             }
         }
     }, []);
 
     const manuscript = <div>
-        <canvas ref={canvasRef} id="canvas" width="200" height="200"></canvas>
+        <canvas ref={canvasRef} id="canvas" width="1000" height="1000"></canvas>
     </div>;
 
     return manuscript;

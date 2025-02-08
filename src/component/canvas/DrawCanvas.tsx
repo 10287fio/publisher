868325966f863b9aac7154cf21b9720dff771c35
10287fio <10287fio@gmail.com>
2025-02-08 17:57:32 +0900

    .

--- src/component/canvas/DrawCanvas.tsx
+++ src/component/canvas/DrawCanvas.tsx
@@ -78,6 +78,7 @@ const DrawCanvas = ({shapeStateProps, updateShapeStateProps}: CanvasComponentPro
                             drawCtx.beginPath();
                             drawCtx.arc(setX, setY, 5, 0, 2 * Math.PI);
                             drawCtx.fill();
+
                         } else if (current?.tool == ToolEnum.Circle) {
                             radius = Math.sqrt((curPoint.x - prePoint.x) ** 2 + (prePoint.y - curPoint.y) ** 2);
 
@@ -113,6 +114,7 @@ const DrawCanvas = ({shapeStateProps, updateShapeStateProps}: CanvasComponentPro
                         let offsetY: number = event.nativeEvent.offsetY;
                         let setX: number = offsetX;
                         let setY: number = offsetY;
+                        let radius: number = 0;
 
                         let foundPoint: Point | undefined = point.findLast(p => p.id == current?.cur_point_id);
 
@@ -124,33 +126,39 @@ const DrawCanvas = ({shapeStateProps, updateShapeStateProps}: CanvasComponentPro
                         let curPoint: { x: number, y: number } = {x: offsetX, y: offsetY};
 
                         if (prePoint != undefined) {
-                            if (current?.operation == OperationEnum.AP_Preset) {
-                                if (shapeUtil.calQuadCoord(prePoint, curPoint) == "x") {
-                                    setX = curPoint.x;
-                                    setY = prePoint.y;
-                                } else if (shapeUtil.calQuadCoord(prePoint, curPoint) == "y") {
-                                    setX = prePoint.x;
-                                    setY = curPoint.y;
+                            if (current?.tool == ToolEnum.Line) {
+                                if (current?.operation == OperationEnum.AP_Preset) {
+                                    if (shapeUtil.calQuadCoord(prePoint, curPoint) == "x") {
+                                        setX = curPoint.x;
+                                        setY = prePoint.y;
+                                    } else if (shapeUtil.calQuadCoord(prePoint, curPoint) == "y") {
+                                        setX = prePoint.x;
+                                        setY = curPoint.y;
+                                    }
                                 }
+
+                                setPoint((prevPoints: PointArray) => [...prevPoints, {
+                                    id: pointId,
+                                    shape_id: current?.shape_id,
+                                    x: setX,
+                                    y: setY,
+                                    is_deleted: false,
+                                    to_close: false
+                                }]);
+
+                                setCurrent((prevState: Current) => ({
+                                    ...prevState,
+                                    cur_point_id: pointId,
+                                    pre_point_id1: prevState.cur_point_id,
+                                    pre_point_id2: prevState.pre_point_id1,
+                                    pre_point_id3: prevState.pre_point_id2
+                                }));
+
+                            } else if (current?.tool == ToolEnum.Circle) {
+
+
                             }
                         }
-
-                        setPoint((prevPoints: PointArray) => [...prevPoints, {
-                            id: pointId,
-                            shape_id: current?.shape_id,
-                            x: setX,
-                            y: setY,
-                            is_deleted: false,
-                            to_close: false
-                        }]);
-
-                        setCurrent((prevState: Current) => ({
-                            ...prevState,
-                            cur_point_id: pointId,
-                            pre_point_id1: prevState.cur_point_id,
-                            pre_point_id2: prevState.pre_point_id1,
-                            pre_point_id3: prevState.pre_point_id2
-                        }));
                     }
                 }
             }
